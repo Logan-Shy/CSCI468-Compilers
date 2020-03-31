0fba6ed2263eea206d1b0a154d44004a0d0c122c
Logan Shy <loganshy740@gmail.com>
2020-03-31 13:34:29 -0600

    removed unnecessary echo statements from micro

--- step3/Little/Micro.sh
+++ step3/Little/Micro.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 #micro execution script
 
-echo "running antlr on g4 file..."
 antlr4 Little.g4
 
-echo "compiling generated files..."
 javac Little*.java
 
-echo "executing driver file with given argument..."
 java LittleDriver $1
