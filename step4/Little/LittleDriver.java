2dc83ca7065b95f50c9b5cac73a1e9d12cb85c03
Logan Shy <loganshy740@gmail.com>
2020-04-23 21:45:51 -0600

    started 3AC generation and further implemented AST generation

--- step4/Little/LittleDriver.java
+++ step4/Little/LittleDriver.java
@@ -10,7 +10,6 @@ import java.nio.file.Paths;
 import java.util.*;
 
 import javax.management.RuntimeErrorException;
-import javax.sound.sampled.SourceDataLine;
 
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import org.antlr.v4.runtime.*;
@@ -73,11 +72,42 @@ public class LittleDriver {
             System.out.println("-----------------");
         }
         System.out.println("\nFinal Trees: ");
-        while(!listener.treeList.isEmpty()){
-            AstNode.Print(listener.treeList.removeFirst());
+        LinkedList<AstNode> printList = new LinkedList<AstNode>();
+        printList = (LinkedList<AstNode>) listener.treeList.clone();
+        while(!printList.isEmpty()){
+            AstNode.Print(printList.removeFirst());
             System.out.println("-----------------");
         }
 
+        //Generate code
+        System.out.println("\nGenerating Code from trees:");
+        Stack<CodeObject> codeStack = new Stack<CodeObject>();
+        while(!listener.treeList.isEmpty()){
+            AstNode.GenerateCode(listener.treeList.removeFirst(), codeStack);
+            while(!codeStack.empty()){//print code object for given AST
+                CodeObject.Print(codeStack.pop());
+                System.out.println("______________________");
+            }
+        }
+
+    }
+}
+
+class CodeObject{
+    String temp;
+    String type;
+    String code;
+
+    public CodeObject(String temp, String type, String code){
+        this.temp = temp;
+        this.type = type;
+        this.code = code;
+    }
+
+    public static void Print(CodeObject o){
+        System.out.println("Temp name: "+ o.temp);
+        System.out.println("Type:"+ o.type);
+        System.out.println("Generated Code:\n" + o.code);
     }
 }
 
@@ -137,11 +167,162 @@ class AstNode{
             }
         }
     }
+
+    public static void GenerateCode(AstNode head, Stack<CodeObject> generatedCode){
+        if(head != null){
+            if(head.Lchild != null){
+                GenerateCode(head.Lchild, generatedCode);
+            }
+            if(head.Rchild != null){
+                GenerateCode(head.Rchild, generatedCode);
+            }
+
+
+            if(head.type.contains("AssignExpr")){
+                //generate new temp for name
+                String tempName = CreateTemporary();
+                String code = "";
+                String typeName = "";
+
+                //pull up rhs and lhs code objects
+                CodeObject assignee = generatedCode.pop();
+                CodeObject assigner = generatedCode.pop();
+
+                if(assignee.code != null){
+                    code += assignee.code;
+                }
+                if(assignee.type.contains("INT")){
+                    code += "STOREI" + " " + assignee.temp + " " + assigner.temp + "\n";
+                    typeName = "INT";
+                } else if(assignee.type.contains("FLOAT")){
+                    code += "STOREF" + " " + assignee.temp + " " + assigner.temp + "\n";
+                    typeName = "FLOAT";
+                }
+                CodeObject assignObject = new CodeObject(tempName, typeName, code);
+                generatedCode.push(assignObject);
+            } else if(head.type.contains("Literal")){//float or int
+                if(head.type.contains("Int")){
+                    //create new code object with only two fields filled in
+                    //Literal
+                    CodeObject newObject = new CodeObject(head.data[0], "INTLIT", null);
+                    generatedCode.push(newObject);
+                } else if(head.type.contains("Float")){
+                    //Float
+                    CodeObject newObject = new CodeObject(head.data[0], "FLOATLIT", null);
+                    generatedCode.push(newObject);
+                }
+            } else if(head.type.contains("VarRef")){//Var Reference
+                CodeObject newObject = new CodeObject(head.data[0], head.data[1], null);
+                generatedCode.push(newObject);
+            } else if(head.type.contains("AddExpr")){//Add Expression
+                //Generate new temp for name
+                String tempName = CreateTemporary();
+                String typeName = "";
+                String code = "";
+                String leftTemp = "[LEFT TEMP NAME]";
+                String rightTemp = "[RIGHT TEMP NAME]";
+
+                //Guaranteed two nodes from left and right child to be added
+                CodeObject rightObject = generatedCode.pop();
+                CodeObject leftObject = generatedCode.pop();
+
+                //Left child code generation
+                if(!leftObject.temp.contains("$T") && !leftObject.type.contains("LIT")){//var reference
+                    leftTemp = leftObject.temp;
+                    code += "LD " + leftObject.temp + " " + CreateTemporary() + "\n";
+                } else if(leftObject.type.contains("LIT")){//Int Literal
+                    leftTemp = leftObject.temp;
+                } else if(leftObject.temp.contains("$T")){//expression
+                    //load generated code from expression and add to new code
+                    leftTemp = leftObject.temp;
+                    code += leftObject.code;
+                }
+
+                //Right Child code generation
+                if(!rightObject.temp.contains("$T") && !rightObject.type.contains("LIT")){//var reference
+                    rightTemp = rightObject.temp;
+                    code += "LD " + rightObject.temp + " " + CreateTemporary() + "\n";
+                } else if(rightObject.type.contains("LIT")){//Int Literal
+                    rightTemp = rightObject.temp;
+                } else if(rightObject.temp.contains("$T")){//expression
+                    //load generated code from expression and add to new code
+                    rightTemp = rightObject.temp;
+                    code += rightObject.code;
+                }
+
+
+                //Finalize Add expr Code generation
+                if(head.type.contains("+")){//use Add instruction variant
+                    if(leftObject.type.contains("INT")){//Use AddI instruction
+                        code += "ADDI "+ leftTemp +" "+ rightTemp +" "+ tempName +"\n";
+                        typeName = "INT";
+                    } else if(leftObject.type.contains("FLOAT")){//Use AddF instruction
+                        code += "ADDF "+ leftTemp +" "+ rightTemp +" "+ tempName +"\n";
+                        typeName = "FLOAT";
+                    }
+                } else if(head.type.contains("-")){//Use Sub instruction variant
+                    if(leftObject.type.contains("INT")){//Use SubI instruction
+                        code += "SUBI "+ leftTemp +" "+ rightTemp +" "+ tempName +"\n";
+                        typeName = "INT";
+                    } else if(leftObject.type.contains("FLOAT")){//Use SubF instruction
+                        code += "SUBF "+ leftTemp +" "+ rightTemp +" "+ tempName +"\n";
+                        typeName = "FLOAT";
+                    }
+                }
+                
+                //Create new Code Object and push up
+                // System.out.println("CREATED CODE:\n" + code);
+                CodeObject AddExpr = new CodeObject(tempName, typeName, code);
+                generatedCode.push(AddExpr);
+            } else if (head.type.contains("VarDecl")){//variable declaration
+                String tempName = head.data[0];
+                String typeName = head.data[1];
+                String code = "var "+tempName;
+                CodeObject varObject = new CodeObject(tempName, typeName, code);
+                generatedCode.push(varObject);
+            } else if(head.type.contains("StringDecl")){//String declaration
+                String strName = head.data[0];
+                String strVal = head.data[1];
+                String code = "str " + strName + " " + strVal;
+                CodeObject strObject = new CodeObject(strName, "STRING", code);
+                generatedCode.push(strObject);
+            } else if(head.type.contains("WriteExpr")){//write statement
+                String tempName = "WRITE";
+                String typeName = "";
+                String code = "";
+                if(head.data[1].contains("\"")){//write string
+                    typeName = "STRING";
+                    code = "sys writes " + head.data[0];
+                    CodeObject strObject = new CodeObject(tempName, typeName, code);
+                    generatedCode.push(strObject);
+                } else{//float or integer
+                    if(head.data[1].contains("INT")){//write int
+                        typeName = head.data[1];
+                        code = "sys writei " + head.data[0];
+                    } else if(head.data[1].contains("FLOAT")){//write float
+                        typeName = head.data[1];
+                        code = "sys writer " + head.data[0];
+                    } else{
+                        System.out.println("SOMETHING WACK JUST HAPPENED");
+                    }
+                    CodeObject varObject = new CodeObject(tempName, typeName, code);
+                    generatedCode.push(varObject);
+                }
+            }
+        }
+    }
+
+    public static String CreateTemporary(){
+        MyLittleListener.tempCounter++;
+        String temporary = "$T" + MyLittleListener.tempCounter;
+        return temporary;
+    }
 }
 
 
 class MyLittleListener extends LittleBaseListener{
     
+    public static int tempCounter = 0;
     Stack<AstNode> nodePool = new Stack<AstNode>();
     LinkedList<AstNode> treeList = new LinkedList<AstNode>();
     // Init new symbole table
@@ -163,6 +344,38 @@ class MyLittleListener extends LittleBaseListener{
     }
 
     ///////// These functions handle AST generation \\\\\\\\\\\\\\\
+    @Override
+    public void exitWrite_stmt(LittleParser.Write_stmtContext ctx){
+        LinkedHashMap<String,String> symbolTable = this.finalTable.get("GLOBAL");
+        //Check if only one variable to write
+        if(ctx.id_list().id_tail().getChildCount() <= 0){//only one
+            String varId = ctx.id_list().id().getStart().getText();
+            String varType = symbolTable.get(varId);
+            AstNode writeop = new AstNode(null, null, "WriteExpr", new String[] {varId, varType});
+            this.treeList.add(writeop);
+        } else{//more than one var to write
+            //Write id in id_list()
+            String varId = ctx.id_list().id().getStart().getText();
+            String varType = symbolTable.get(varId);
+            AstNode writeop = new AstNode(null, null, "WriteExpr", new String[] {varId, varType});
+            this.treeList.add(writeop);
+            //Write subsequent ids in id_tails()
+            LittleParser.Id_tailContext tailCtx = ctx.id_list().id_tail();
+            while(true){
+                varId = tailCtx.id().getStart().getText();
+                varType = symbolTable.get(varId);
+                AstNode nextWriteop = new AstNode(null, null, "WriteExpr", new String[] {varId, varType});
+                this.treeList.add(nextWriteop);
+                if(tailCtx.id_tail().getChildCount() > 0){//more to print
+                    tailCtx = tailCtx.id_tail();
+                } else{//No more to print, break out
+                    break;                    
+                }
+            }
+
+        }
+    }
+    
     @Override
     public void exitAddop(LittleParser.AddopContext ctx){
         //Add correct node depending on text of addop
@@ -470,25 +683,31 @@ class MyLittleListener extends LittleBaseListener{
     @Override
     public void exitVar_decl(LittleParser.Var_declContext ctx){
         String type = ctx.var_type().getStart().getText();
+        String varName = ctx.id_list().getStart().getText();
         LinkedHashMap<String, String> scope = this.scopeStack.peek();//get current scope
         LittleParser.Id_tailContext commaCtx = ctx.id_list().id_tail();
-        //put first var id into table
-        if(!(scope.containsKey(ctx.id_list().getStart().getText()))){
-            scope.put(ctx.id_list().getStart().getText(), type);
+        //put first var id into table and create first AST node
+        if(!(scope.containsKey(varName))){
+            scope.put(varName, type);
+            AstNode varNode = new AstNode(null, null, "VarDecl", new String[] {varName, type});
+            this.treeList.push(varNode);
             while(commaCtx.getStart().getText().contains(",")){
                 //get next variable id's if any exist and not already declared
-                if(!(scope.containsKey(commaCtx.id().getStart().getText()))){
-                    scope.put(commaCtx.id().getStart().getText(), type);
+                varName = commaCtx.id().getStart().getText();
+                if(!(scope.containsKey(varName))){
+                    scope.put(varName, type);
+                    varNode = new AstNode(null, null, "VarDecl", new String[] {varName, type});
+                    this.treeList.push(varNode);
                     commaCtx = commaCtx.id_tail();
                 } else{//duplicate declaration found
                     if(this.errorVar.isEmpty()){
-                        this.errorVar = commaCtx.id().getStart().getText();
+                        this.errorVar = varName;
                     }
                 }
             }
         } else{
             if(this.errorVar.isEmpty()){
-                this.errorVar = ctx.id_list().getStart().getText();
+                this.errorVar = varName;
             }
         }
     }
@@ -497,11 +716,15 @@ class MyLittleListener extends LittleBaseListener{
     public void exitString_decl(LittleParser.String_declContext ctx){
         LinkedHashMap<String,String> scope = this.scopeStack.peek();//get current scope
         //add string identifier/value pair to current scope table
-        if(!(scope.containsKey(ctx.id().getStart().getText()))){
-            scope.put(ctx.id().getStart().getText(), "STRING value " + ctx.str().getStart().getText());
+        String strName = ctx.id().getStart().getText();
+        String strVal = ctx.str().getStart().getText();
+        if(!(scope.containsKey(strName))){
+            scope.put(strName,  strVal);
+            AstNode stringNode = new AstNode(null, null, "StringDecl", new String[] {strName, strVal});
+            this.treeList.push(stringNode);
         } else {
             if(this.errorVar.isEmpty()){
-                this.errorVar = ctx.id().getStart().getText();
+                this.errorVar = strName;
             }
         }
     }
